2645dc5283e8c5b8e88f358d5aeaeedcb02b6e22
aojha5657 <anuragojha497@gmail.com>
2025-06-06 17:46:31 +0530

    git config 2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,37 +7,36 @@ const { configs: eslintConfigs } = eslintJs
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {
-    ignores: ['dist', 'node_modules'],
-  },
-  {
-    files: ['**/*.ts'],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: path.resolve(),
-      },
+    {
+        ignores: ['dist', 'node_modules'],
     },
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                project: './tsconfig.json',
+                tsconfigRootDir: path.resolve(),
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+        },
+        rules: {
+            ...tseslint.configs.recommendedTypeChecked[0].rules,
+            'no-console': 'warn',
+            'dot-notation': ['error', { allowKeywords: true }],
+        },
     },
-    rules: {
-      ...tseslint.configs.recommendedTypeChecked[0].rules,
-      'no-console': 'warn',
-      'dot-notation': ['error', { allowKeywords: true }]
-    }
-  },
-  {
-    files: ['**/*.js', '**/*.mjs'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+    {
+        files: ['**/*.js', '**/*.mjs'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+        rules: {
+            'no-console': 'off',
+            'dot-notation': ['error', { allowKeywords: true }],
+        },
     },
-    rules: {
-      'no-console': 'off',
-      'dot-notation': ['error', { allowKeywords: true }]
-
-    }
-  }
 ]
