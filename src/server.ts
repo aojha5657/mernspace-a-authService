a6eae0f2f82b901ab7f56b2478bb75832f3c945e
aojha5657 <anuragojha497@gmail.com>
2025-06-06 17:45:14 +0530

    add config

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,2 @@
-function showUser(): void {
-    const user = {
-      name: 'Anurag',
-      age: 25
-    };
-  
-    const userName = user.name;
-    const userAge = user.age;
-  
-    console.log(userName, userAge);
-  }
-  
-  showUser();
-  
+import { Config } from './config'
+console.log(Config.PORT)
