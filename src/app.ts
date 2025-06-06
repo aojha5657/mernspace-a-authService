f9547645a0a08a21b1262dbe2452172908fb8bce
aojha5657 <anuragojha497@gmail.com>
2025-06-07 01:51:14 +0530

    http error handling

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,33 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import logger from './config/logger'
+import createHttpError, { HttpError } from 'http-errors'
+
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('welcome to the auth service')
+// Route Handler
+app.get('/', (req: Request, res: Response) => {
+    const err = createHttpError(401, 'You can not access this route.')
+    throw err
+    res.send('Welcome to auth service')
+})
+
+// Global Error Handler
+ 
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message)
+
+    const statusCode = err.statusCode || 500
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: '',
+                location: '',
+            },
+        ],
+    })
 })
 
 export default app
