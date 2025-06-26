cc79f49df9bee80546dde0a8f88c784cb3a7b15e
aojha5657 <anuragojha497@gmail.com>
2025-06-26 11:23:17 +0530

    abc

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,6 @@ app.get('/', async (req, res) => {
 
 app.use('/auth', authRouter)
 
- 
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
     logger.error(err.message)
     const statusCode = err.statusCode || 500
